483778a387651cc3c3fb2cc1baff2c54a8d19bd6
opilane <opilane@HP-CZC6297BBL>
2020-11-16 18:38:07 +0200

    kod

--- kk/kk/MainPage.xaml.cs
+++ kk/kk/MainPage.xaml.cs
@@ -10,7 +10,9 @@ namespace kk
 {
     public partial class MainPage : ContentPage
     {
+        DatePicker dpicker;
         Picker puker, piker;
+        Image nin;
         public MainPage()
         {
             Grid maakond = new Grid
@@ -19,15 +21,23 @@ namespace kk
                 {
                     new RowDefinition{Height=new GridLength(1,GridUnitType.Star)},
                     new RowDefinition{Height=new GridLength(1,GridUnitType.Star)},
+                    new RowDefinition{Height=new GridLength(1,GridUnitType.Star)},
+                    new RowDefinition{Height=new GridLength(1,GridUnitType.Star)},
+                    new RowDefinition{Height=new GridLength(1,GridUnitType.Star)},
                     new RowDefinition{Height=new GridLength(1,GridUnitType.Star)}
                 },
                 ColumnDefinitions =
                 {
                     new ColumnDefinition{Width=new GridLength(1,GridUnitType.Star)},
-                    new ColumnDefinition{Width=new GridLength(1,GridUnitType.Star)}
 
                 }
-            };
+
+
+        };
+
+            Label bika = new Label { Text = "Инфо. для доставки", TextColor = Color.White, BackgroundColor= Color.CornflowerBlue, FontSize = 35};
+            Grid.SetColumnSpan(bika, 2);
+            maakond.Children.Add(bika, 0, 0);
 
             puker = new Picker
             {
@@ -35,7 +45,6 @@ namespace kk
             };
             puker.Items.Add("ИДА-ВИРУМАА");
             puker.Items.Add("ЙЫГЕВАМАА");
-            piker.Items.Add("v");
             puker.Items.Add("ЛЯЭНЕМАА");
             puker.Items.Add("ЛЯЭНЕ-ВИРУМАА");
             puker.Items.Add("ПЫЛВАМАА");
@@ -49,14 +58,14 @@ namespace kk
             puker.Items.Add("ВЫРУМАА");
             puker.Items.Add("ВИЛЬЯНДИМАА");
             puker.Items.Add("ВАЛГАМАА");
-            maakond.Children.Add(puker, 0, 0);
+            maakond.Children.Add(puker, 0, 1);
             puker.SelectedIndexChanged += Puker_SelectedIndexChanged;
 
             piker = new Picker
             {
                 Title = "Центр уезда"
             };
-            piker.Items.Add("ИДА-ВИРУМАА");
+            piker.Items.Add("Нарва");
 
             piker.Items.Add("ЙЫГЕВАМАА");
             piker.Items.Add("ЛЯЭНЕМАА");
@@ -72,22 +81,38 @@ namespace kk
             piker.Items.Add("ВЫРУМАА");
             piker.Items.Add("ВИЛЬЯНДИМАА");
             piker.Items.Add("ВАЛГАМАА");
-            maakond.Children.Add(piker, 0, 0);
+            maakond.Children.Add(piker, 0, 2);
 
+           Editor editor = new Editor { Placeholder = "Ваш почтовый индекс:" };
+            maakond.Children.Add(editor, 0, 3);
+
+            Image nin = new Image { };
+            maakond.Children.Add(nin, 0, 4);
+
+            Button oform = new Button { Text = "Заказать", BorderColor = Color.CornflowerBlue };
+            oform.Clicked += Oform_Clicked;
+            maakond.Children.Add(oform, 0, 5);
+
+            Content = maakond;
+
+        }
+
+        private async void Oform_Clicked(object sender, EventArgs e)
+        {
+            await DisplayAlert("Потвердить заказ ", "Ваш Уезд \n" + puker.SelectedItem.ToString() + "\nЦентр уезда\n" + piker.SelectedItem.ToString() + "\nДанные скрыты\n", "Заказать", "Отмена");
 
         }
 
         private void Puker_SelectedIndexChanged(object sender, EventArgs e)
         {
-            string text = puker.SelectedItem.ToString();
-            switch (puker.Title)
+
+            piker.SelectedIndex = puker.SelectedIndex;
+            puker.SelectedIndex = piker.SelectedIndex;
+
+            if (piker.SelectedIndex == 0)
             {
-                    case "ИДА-ВИРУМАА":
-                              piker.SelectedItem;
-                break;
+                nin.Source = im "pp.png";
             }
-
-
         }
     }
 }
